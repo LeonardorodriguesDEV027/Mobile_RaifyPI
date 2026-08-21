fb79a274265b1ce3ea64542b38448ef1d7e81766
20241tiimi0119 <luigiknup77@gmail.com>
2026-08-21 18:19:53 -0300

    tela login 2/3 pronto

---
+++ app/src/main/java/com/example/tcc_raify/AreaTalhoes.kt
@@ -0,0 +1,20 @@
+package com.example.tcc_raify
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AreaTalhoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_area_talhoes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
