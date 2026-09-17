8876774bc33f33b918cf815e311beff8cece5aa9
20241tiimi0119 <luigiknup77@gmail.com>
2026-09-17 17:06:03 -0300

    tela codigo senha e tela nova senha (apenas xml) (falta kotlin)

--- app/src/main/java/com/example/tcc_raify/activity_codigosenha.kt
+++ app/src/main/java/com/example/tcc_raify/activity_codigosenha.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AreaTalhoes : AppCompatActivity() {
+class activity_codigosenha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_area_talhoes)
+        setContentView(R.layout.activity_codigosenha)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
